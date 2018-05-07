9d55982c6efaea8529ee82a68c52bca8945f86d9
Breno Henrique Ferreira Franco <breno.franco@cabal.com.br>
2018-05-07 13:22:25 -0300

    Initial commit

---
+++ src/main/java/br/com/medonline/MedOnlineApplication.java
@@ -0,0 +1,12 @@
+package br.com.medonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedOnlineApplication.class, args);
+	}
+}

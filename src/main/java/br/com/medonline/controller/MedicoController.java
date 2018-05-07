7eb2f284f174d959286732e903263f2834b4a24d
Breno Henrique Ferreira Franco <breno.franco@cabal.com.br>
2018-05-07 16:28:36 -0300

    Adicionando método para buscar medico pelo id e colocando restController

--- src/main/java/br/com/medonline/controller/MedicoController.java
+++ src/main/java/br/com/medonline/controller/MedicoController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.medonline.modal.Medico;
 import br.com.medonline.service.MedicoService;
 
+@RestController
 @RequestMapping("/medico")
 public class MedicoController {
 
@@ -24,6 +27,11 @@ public class MedicoController {
 	@RequestMapping(value = "/salvar", method = RequestMethod.POST)
 	public void salvar(@RequestBody Medico medico){
 		service.salvarMedico(medico);
+	}
+	
+	@RequestMapping(value = "/buscaMedicoPeloID")
+	public Medico getByFaceID(@RequestParam(value="idMedico") Long idMedico) {
+		return service.buscaMedicoPorID(idMedico);
 	}
 
 }

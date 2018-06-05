b0d0ed5ce212d71cc3e0b6a57bb82f492044ee40
Breno Henrique Ferreira Franco <brenohff@gmail.com>
2018-06-05 15:07:51 -0300

    Adicionando banco de dados.

--- src/main/java/br/com/medonline/MedOnlineApplication.java
+++ src/main/java/br/com/medonline/MedOnlineApplication.java
@@ -1,23 +1,14 @@
 package br.com.medonline;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.medonline.modal.Endereco;
-import br.com.medonline.modal.Paciente;
-import br.com.medonline.modal.TipoSexo;
-import br.com.medonline.service.PacienteService;
-
 @SpringBootApplication
 public class MedOnlineApplication implements CommandLineRunner {
 
-	@Autowired
-	private PacienteService service;
+//	@Autowired
+//	private PacienteService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedOnlineApplication.class, args);
@@ -25,23 +16,23 @@ public class MedOnlineApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Paciente p = new Paciente();
-		p.setEmail("brenohff@gmail.com");
-		p.setNome("Breno Henrique Ferreira Franco");
-		p.setSexo(TipoSexo.MASC);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		p.setDtNascimento((Date) dateFormat.parse("22/05/1996"));
-
-		Endereco e = new Endereco();
-		e.setCep("72320-301");
-		e.setLogradouro("QI 416 conjunto 1 bl. A apto. 1204");
-		e.setComplemento("Residencial das Palmeiras");
-		
-		p.setEndereco(e);
-		p.setObservacao("Paciente com problemas no joelho.");
-		
-		
-		service.salvarPaciente(p);
+//		Paciente p = new Paciente();
+//		p.setEmail("brenohff@gmail.com");
+//		p.setNome("Breno Henrique Ferreira Franco");
+//		p.setSexo(TipoSexo.MASC);
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+//		p.setDtNascimento((Date) dateFormat.parse("22/05/1996"));
+//
+//		Endereco e = new Endereco();
+//		e.setCep("72320-301");
+//		e.setLogradouro("QI 416 conjunto 1 bl. A apto. 1204");
+//		e.setComplemento("Residencial das Palmeiras");
+//		
+//		p.setEndereco(e);
+//		p.setObservacao("Paciente com problemas no joelho.");
+//		
+//		
+//		service.salvarPaciente(p);
 
 	}
 }

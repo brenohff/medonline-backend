be2f5a040695ce6946e135f8d41cc5188a2b8fa2
Breno Henrique Ferreira Franco <breno.franco@cabal.com.br>
2018-06-06 15:46:17 -0300

    Removendo metodo q adiciona medico automaticamente.

--- src/main/java/br/com/medonline/MedOnlineApplication.java
+++ src/main/java/br/com/medonline/MedOnlineApplication.java
@@ -1,23 +1,12 @@
 package br.com.medonline;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.medonline.modal.Endereco;
-import br.com.medonline.modal.Medico;
-import br.com.medonline.modal.TipoSexo;
-import br.com.medonline.service.EspecialidadeService;
-import br.com.medonline.service.MedicoService;
-
 @SpringBootApplication
 public class MedOnlineApplication implements CommandLineRunner {
 
-	@Autowired
-	private MedicoService medService;
-	@Autowired
-	private EspecialidadeService espService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedOnlineApplication.class, args);
@@ -25,19 +14,6 @@ public class MedOnlineApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Medico m = new Medico();
-		m.setNome("Gregory House");
-		m.setCrm("22051996");
-		m.setSexo(TipoSexo.MASC);
-		m.setEspecialidade(espService.buscaEspecialidadePorID(492l));
-		
-		Endereco e = new Endereco();
-		e.setCep("70123-456");
-		e.setLogradouro("SCS Bloco 6 Ed. Sofia");
-		e.setComplemento("2 andar");
-		
-		m.setEndereco(e);
-		medService.salvarMedico(m);
 		
 	}
 }
